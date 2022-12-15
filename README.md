037269a8ded2748fc9554b56e010563d5413ac28
Charles Walker <cwalker@magnite.com>
2022-12-15 16:01:34 -0700

    adds selenium drivers and first test

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # hotel-engine-tests
 Take Home Assignment for Hotel Engine SDET Interview
+
+
+1. source venv/bin/activate
+2. python3 -m pip install --upgrade pip
+3. 
--- search_test.py
+++ search_test.py
@@ -1,17 +1,33 @@
+import chromedriver_autoinstaller
+import geckodriver_autoinstaller
+import pytest
 import requests
-import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 
-token = "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"
-url = "https://api.github.com/search/repositories"
+
+github_search_api = "https://api.github.com/search/repositories"
 query_param = "?q="
 query = "create-react-app"
 
-response = requests.get(f"{url}{query_param}{query}", 
-    headers = {"Accept": "application/vnd.github+json", 
-               "Authorization": f"Bearer {token}", 
+response = requests.get(f"{github_search_api}{query_param}{query}", 
+    headers = {"Accept": "application/vnd.github+json",
                "X-GitHub-Api-Version": "2022-11-28"})
 
 payload = response.json()
+url = payload['items'][0]['html_url']
 
-print(payload['items'][0]['url'])
+geckodriver_autoinstaller.install()
+chromedriver_autoinstaller.install()
+
+driver = webdriver.Chrome()
+driver.get(url)
+
+expected_about_text = "Set up a modern web app by running one command."
+
+about = driver.find_elements(By.CSS_SELECTOR, "p.f4.my-3")
+assert about[0].text == expected_about_text
+
+driver.close()
